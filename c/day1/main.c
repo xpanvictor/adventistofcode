cdfc767f64891d04dd75e419a241f14edd49a2d0
xpanvictor <xpanvictor@gmail.com>
2023-09-29 13:19:41 +0100

    init: adventist of code with centre of c

---
+++ c/day1/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+
+// - fetch calories
+// - method to split elves records
+// - method to sum up elves data
+// - data sorting
+
+int main() {
+  printf("Hello world\n");
+  return 0;
+}
+
