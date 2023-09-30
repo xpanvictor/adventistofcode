94b6fd2cee75bdf59b4d45bc080051c03e831a24
xpanvictor <xpanvictor@gmail.com>
2023-09-30 10:03:04 +0100

    feat(day1): part one solution

--- rust/day1/main.rs
+++ rust/day1/main.rs
@@ -1,6 +1,4 @@
-
-use std::{fs, rc::Rc, cell::RefCell};
-
+use std::{cell::RefCell, fs};
 
 #[derive(Debug)]
 struct Elve {
@@ -14,7 +12,7 @@ impl Elve {
         Elve {
             elve_index: index,
             elve_calories: vec![],
-            calories_total: 0
+            calories_total: 0,
         }
     }
 
@@ -25,60 +23,63 @@ impl Elve {
 }
 
 struct ElvesParser {
-    highest_elve_index: Option<usize>,
-    elves: Rc<RefCell<Vec<Elve>>>,
+    highest_elve_vec_index: Option<usize>,
+    elves: RefCell<Vec<Elve>>,
     calories_ledger: String,
 }
 
 impl ElvesParser {
     fn new(ledger_path: &str) -> ElvesParser {
         ElvesParser {
-            elves: Rc::new(RefCell::new(vec![])),
-            highest_elve_index: None,
-            calories_ledger: fs::read_to_string(ledger_path).expect("Couldn't read calories file")
+            elves: RefCell::new(vec![]),
+            highest_elve_vec_index: None,
+            calories_ledger: fs::read_to_string(ledger_path).expect("Couldn't read calories file"),
         }
     }
 
-    fn parse_elves(& mut self) {
+    fn parse_elves(&mut self) {
+        // Create first elve
+        self.elves.borrow_mut().push(Elve::new(1));
         for line in self.calories_ledger.lines() {
-
             // Create new elve if not present
             if line.trim().is_empty() {
                 // check if total is higher than highest and update highest elve
-                if let Some(active_elve) = Rc::clone(&self.elves).borrow().last() {
-                    if let Some(current_highest_elve_index) = self.highest_elve_index {
-                        let current_highest_elve = &self.elves.borrow()[current_highest_elve_index];
+                if let Some(active_elve) = self.elves.borrow().last() {
+                    println!("New elve: {:?}", active_elve);
+
+                    if let Some(current_highest_elve_vec_index) = self.highest_elve_vec_index {
+                        let current_highest_elve = &self.elves.borrow()[current_highest_elve_vec_index];
                         if active_elve.calories_total > current_highest_elve.calories_total {
-                            println!("Highest elve {:#?}", current_highest_elve);
+                            self.highest_elve_vec_index = Some(self.elves.borrow().len() - 1);
                         }
-                        println!("Highest elve {:#?}", current_highest_elve);
                     } else {
-                        self.highest_elve_index = Some(Rc::clone(&self.elves).borrow().len() - 1);
+                        self.highest_elve_vec_index = Some(self.elves.borrow().len() - 1);
                     }
                 }
 
-                Rc::clone(&self.elves).borrow_mut().push(Elve::new((Rc::clone(&self.elves).borrow().len() as u16) + 1));
+                let num_of_elves = (self.elves.borrow().len() as u16) + 1;
+
+                self.elves.borrow_mut().push(Elve::new(num_of_elves));
+
                 continue;
             }
-            
+
             // populate calorie for active elve
-            if let Some(last_elve) = Rc::clone(&self.elves).borrow_mut().last_mut() {
-                last_elve.add_calorie(
-                    line.trim().parse().expect("Calorie not a valid number")
-                )
-            } 
+            if let Some(last_elve) = self.elves.borrow_mut().last_mut() {
+                last_elve.add_calorie(line.trim().parse().expect("Calorie not a valid number"))
+            }
         }
-        
-        let highest_elve = &self.elves.borrow()[self.highest_elve_index.expect("No index")];
-        println!("{:#?} {}", highest_elve, highest_elve.elve_index);
     }
 }
-
 
 fn main() {
     let mut elves_parser = ElvesParser::new("day1/calories.txt");
     elves_parser.parse_elves();
 
+    println!(
+        "Highest elve recorded {:#?}",
+        elves_parser.elves.borrow()[elves_parser.highest_elve_vec_index.expect("No index")]
+    );
+
     println!("Elve solution impl!")
 }
-
